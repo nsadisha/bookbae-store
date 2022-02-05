f3dc790abf9d6b6e31ef3a8a41cb02ddf2d83fc9
nsadisha <nsadisha@gmail.com>
2022-02-05 17:48:43 +0530

    login screen created

---
+++ src/main/java/com/bbstore/ui/UI.java
@@ -0,0 +1,5 @@
+package com.bbstore.ui;
+
+public interface UI {
+
+}
---
+++ src/main/java/com/bbstore/ui/uis/HomeScreen.java
@@ -0,0 +1,7 @@
+package com.bbstore.ui.uis;
+
+import javax.swing.*;
+
+public class HomeScreen {
+    private JPanel panel1;
+}
