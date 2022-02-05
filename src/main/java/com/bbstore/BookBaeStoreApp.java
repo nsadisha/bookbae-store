e4da3ab6801e2e00879cc47f1463f41957d03132
nsadisha <nsadisha@gmail.com>
2022-02-05 17:48:13 +0530

    input validator and usernauthenticator

--- src/main/java/com/bbstore/BookBaeStoreApp.java
+++ src/main/java/com/bbstore/BookBaeStoreApp.java
@@ -2,19 +2,29 @@ package com.bbstore;
 
 import com.bbstore.connection.DBConnection;
 import com.bbstore.database.Database;
+import com.bbstore.input.InputValidator;
+import com.bbstore.ui.uis.LogIn;
+import com.bbstore.users.UserAuthenticator;
 
 public class BookBaeStoreApp {
     DBConnection connection;
     Database database;
+    UserAuthenticator authenticator;
+    InputValidator inputValidator;
 
-    public BookBaeStoreApp(DBConnection connection, Database database){
+    public BookBaeStoreApp(DBConnection connection, Database database, InputValidator inputValidator){
         this.connection = connection;
         this.database = database;
+        this.inputValidator = inputValidator;
     }
 
     public void execute(){
         try {
             database.connect(connection.getConnection());
+            authenticator = new UserAuthenticator(this.database);
+
+            LogIn login = new LogIn(authenticator, this.inputValidator);
+            login.setVisible(true);
 
         }catch (Exception e){
             System.out.println("Error: "+e.getMessage());
