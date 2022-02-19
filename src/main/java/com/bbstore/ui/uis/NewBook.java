310c6353e8b1eebcb750695380c9ab8895ab8913
nsadisha <nsadisha@gmail.com>
2022-02-20 00:13:29 +0530

    add new book with book manager

--- src/main/java/com/bbstore/ui/uis/NewBook.java
+++ src/main/java/com/bbstore/ui/uis/NewBook.java
@@ -1,10 +1,9 @@
 package com.bbstore.ui.uis;
 
-import com.bbstore.database.Database;
-import com.bbstore.input.InputValidator;
+import com.bbstore.books.Book;
+import com.bbstore.books.BookManager;
 import com.bbstore.navigator.Navigator;
 import com.bbstore.ui.GUI;
-import com.bbstore.ui.uis.exceptions.NewBookCreationFailedException;
 
 import javax.swing.*;
 import java.util.Objects;
@@ -25,11 +24,7 @@ public class NewBook extends GUI {
     private JTextField availableQuantityField;
     private JButton cancelButton;
 
-    private final Database database;
-    private final InputValidator inputValidator;
-
-    public NewBook(InputValidator inputValidator,Database database) {
-
+    public NewBook(BookManager bookManager) {
         setSize(700,600);
         setTitle("Add New Book");
         setResizable(false);
@@ -38,9 +33,6 @@ public class NewBook extends GUI {
         setAutoRequestFocus(true);
         setContentPane(this.homePanel);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        this.database= database;
-        this.inputValidator=inputValidator;
 
         addBookButton.addActionListener(e ->{
             String isbn = isbnField.getText();
@@ -56,18 +48,20 @@ public class NewBook extends GUI {
             String bookDescription=bookDescriptionField.getText();
 
             try{
-                boolean isNewBookCreated=addNewBook(
-                        isbn,
-                        bookName,
-                        authorName,
-                        publisherName,
-                        year,
-                        price,
-                        edition,
-                        availableQuantity,
-                        language,
-                        category,
-                        bookDescription
+                boolean isNewBookCreated = bookManager.addNewBook(
+                        new Book(
+                                isbn,
+                                bookName,
+                                authorName,
+                                publisherName,
+                                year,
+                                price,
+                                edition,
+                                availableQuantity,
+                                language,
+                                category,
+                                bookDescription
+                        )
                 );
 
                 if(isNewBookCreated){
@@ -78,40 +72,5 @@ public class NewBook extends GUI {
             }
         });
         cancelButton.addActionListener(e -> Navigator.pop());
-    }
-
-    private boolean addNewBook(String isbn,String bookName,String authorName,String publisherName,String year,String price,String edition,
-    String availableQuantity,String language,String category,String bookDescription) throws Exception{
-
-        if(!inputValidator.validateField(isbn)){
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(bookName)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(authorName)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(publisherName)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(year)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(price)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(edition)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(availableQuantity)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else if(!inputValidator.validateField(bookDescription)) {
-            throw new NewBookCreationFailedException("There should be at least 1 character ");
-        }else {
-            String query = "INSERT INTO books VALUES('"+isbn+ "','" +bookName+ "','" +price+ "','" +
-                    category+ "','" +language+ "','" +authorName+ "','" +year+ "','" +edition
-                    + "','" +publisherName+ "','" +bookDescription+ "','" +availableQuantity+ "')";
-
-            try {
-                int addBook = database.updateQuery(query);
-                return addBook == 1;
-            }catch (Exception e){
-                throw new NewBookCreationFailedException("New book creation failed!: "+e.getMessage());
-            }
-        }
     }
 }
