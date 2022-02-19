62f8382bab7d81397ba43b1149a57a952c721b5b
nsadisha <nsadisha@gmail.com>
2022-02-19 22:58:46 +0530

    ui exceptions

---
+++ src/main/java/com/bbstore/ui/uis/exceptions/NewBookCreationFailedException.java
@@ -0,0 +1,7 @@
+package com.bbstore.ui.uis.exceptions;
+
+public class NewBookCreationFailedException extends Exception {
+    public NewBookCreationFailedException(String msg) {
+        super(msg);
+    }
+}
